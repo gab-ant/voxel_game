8abb896707c391732a97974c91d570654e9a210d
gab-ant <gabrielantonsson.ga@gmail.com>
2026-03-05 20:37:58 +0100

    First triangle

--- src/main.cpp
+++ src/main.cpp
@@ -50,12 +50,6 @@ int main()
     glViewport(0, 0, 800, 600);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
-    };
     
     const char *vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
@@ -81,14 +75,7 @@ int main()
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-    
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+ 
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
 
@@ -98,21 +85,48 @@ int main()
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+   
+    float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+    };
 
-    glUseProgram(shaderProgram);
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
 
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
 
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+ 
+    // location of vertex attrib at location = 0
+    // vec3 (size 3)
+    // data type (vec3 of float)
+    // normalize data
+    // stride, space between consecutive vertex attributes, 0 to autospace
+    // Offset of where position data begins in buffer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+   
     while(!glfwWindowShouldClose(window)) 
     {
 	processInput(window);
 	
-	// Set state
-	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-	// Use state to clear
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glfwPollEvents();
+	// Set state
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
 	glfwSwapBuffers(window);
+	glfwPollEvents();
     }
 
     glfwTerminate();
